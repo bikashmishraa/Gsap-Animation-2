883749150199c55950da55b7b3315a4198e37e2a
bikashmishraa <bikash.mishra2079@gmail.com>
2024-01-11 21:14:55 +0545

    gsap project-2

--- JS/script.js
+++ JS/script.js
@@ -1,45 +1,50 @@
 var tl=gsap.timeline();
+tl.from(".navbar h1",{
+    duration:3,
+    delay:1,
+    opacity:0,
+})
 
-    tl.from(".navbar img",{
-        // x:-1000,
-        duration:4,
-        delay:1,
-        opacity:0,
-    })
-    gsap.from(".navbar ul a",{
-        y:-1000,
-        duration:2,
-        delay:3,
-        stagger:0.5,
-    })
-    
-    gsap.from(".navbar .getstarted",{
-        x:1000,
-        duration:2,
-        delay:3.5,
-    })
-    gsap.from(".firstimg",{
-        x:-1000,
-        duration:2,
-        delay:3,
-        opacity:0,
-    })
-    gsap.from(".secondimg",{
-        y:-1000,
-        duration:2,
-        delay:3,
-        opacity:0,
-    })
-    gsap.from(".thirdimg",{
-        x:-1000,
-        duration:3,
-        delay:3,
-        opacity:0,
-    })
-    gsap.from(".fourthimg",{
-        x:1000,
-        duration:2,
-        delay:3,
-        opacity:0,
-    })
+gsap.from(".navbar ul",{
+    y:-100,
+    duration:2,
+    delay:1,
+})
+gsap.from(".right-nav",{
+    x:1000,
+    duration:3,
+    delay:0.1,
+})
 
+gsap.from(".left",{
+    x:-1000,
+    duration:3,
+    delay:1,
+    opacity:0
+})
+gsap.from(".left h1",{
+    y:1000,
+    x:0,
+    opacity:0,
+    duration:3,
+    delay:1,
+})
+gsap.from(".right img",{
+    x:1000,
+    duration:3,
+    delay:1
+})
+gsap.from(".info,.imagecontainer img",{
+    y:1000,
+    duration:3,
+    delay:0.1,
+    stagger:1,
+    scrollTrigger:{
+        trigger:".page2",
+        scroller:"body",
+        markers:true,
+        start:"top 60%",
+        end:"top 50%",
+        scrub:5,
+    }
+})
